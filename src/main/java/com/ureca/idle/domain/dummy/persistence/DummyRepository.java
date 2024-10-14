f596515876576e82ce6c18e1d85423524d83ebc4
weejinyoung <a8118199@gmail.com>
2024-10-14 17:13:33 +0900

    feat: clear dummy

--- src/main/java/com/ureca/idle/domain/dummy/persistence/DummyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ureca.idle.domain.dummy.persistence;
-
-import com.ureca.idle.domain.dummy.entity.Dummy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DummyRepository extends JpaRepository<Dummy, Long> {
-}
