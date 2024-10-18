54f2a847d8b49049469851e2a8fe82d0ceaccc92
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-10-18 19:03:16 +0900

    Feat: BookException 추가

---
+++ src/main/java/com/ureca/idle/idleapi/idleoriginapi/common/exception/book/BookException.java
@@ -0,0 +1,11 @@
+package com.ureca.idle.idleapi.idleoriginapi.common.exception.book;
+
+import com.ureca.idle.idleapi.idleoriginapi.common.exception.config.BaseException;
+import com.ureca.idle.idleapi.idleoriginapi.common.exception.config.ExceptionType;
+
+public class BookException extends BaseException {
+
+    public BookException(ExceptionType exceptionType) {
+        super(exceptionType);
+    }
+}
