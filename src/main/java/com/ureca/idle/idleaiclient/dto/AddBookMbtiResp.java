e3dc1fa08a98a62e032e52c8ca0446ff7f1371e9
Peindre La Colline <PeindreLaColline@gmail.com>
2024-10-19 13:24:37 +0900

    feat: 책 mbti 관련 dto 추가

---
+++ src/main/java/com/ureca/idle/idleaiclient/dto/AddBookMbtiResp.java
@@ -0,0 +1,17 @@
+package com.ureca.idle.idleaiclient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AddBookMbtiResp {
+    int ei;
+    int sn;
+    int tf;
+    int jp;
+}
