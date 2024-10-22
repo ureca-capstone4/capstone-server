bd710a629e8b5dbf7438715b6a10d32667926d18
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-10-22 17:05:55 +0900

    Refact : Kid 유스 케이스 성향 검사 조회 코드 수정

--- src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/kid/KidManagingUseCase.java
+++ src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/kid/KidManagingUseCase.java
@@ -1,9 +1,14 @@
 package com.ureca.idle.idleapi.idleoriginapi.business.kid;
 
 import com.ureca.idle.idleapi.idleoriginapi.business.kid.dto.*;
+import com.ureca.idle.idleapi.idleoriginapi.business.kid.dto.UpdateKidPersonalityReq;
+import com.ureca.idle.idleapi.idleoriginapi.business.kid.dto.UpdateKidPersonalityResp;
 
 public interface KidManagingUseCase {
     AddKidResp addMyKid(Long userId, AddKidReq req);
     GetKidsProfilesResp getMyKidsProfiles(Long userId);
     GetKidsDetailResp getKidsDetail(Long kidId);
+
+    // 성향 검사 진행 결과 발송
+    UpdateKidPersonalityResp updateKidPersonality(Long kidId, UpdateKidPersonalityReq req);
 }
