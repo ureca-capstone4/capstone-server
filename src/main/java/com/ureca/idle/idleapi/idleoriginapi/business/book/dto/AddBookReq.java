201516be7e652cb03548df83d33c9b5693223495
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-10-22 17:43:09 +0900

    Refact: 주석 제거

--- src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/book/dto/AddBookReq.java
+++ src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/book/dto/AddBookReq.java
@@ -1,4 +1,3 @@
 package com.ureca.idle.idleapi.idleoriginapi.business.book.dto;
 
-// TODO ENUM value validate
 public record AddBookReq(String title, String story, String summary, String author, String publisher, int recommendedAge ) {}
--- src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/book/dto/AddBookResp.java
+++ src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/book/dto/AddBookResp.java
@@ -1,4 +1,3 @@
 package com.ureca.idle.idleapi.idleoriginapi.business.book.dto;
 
-// TODO ENUM value validate
 public record AddBookResp(String title) {}
