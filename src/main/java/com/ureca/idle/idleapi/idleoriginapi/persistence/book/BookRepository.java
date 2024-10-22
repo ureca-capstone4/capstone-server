aa5a9f2245ed32239513e630b33f403a637f9438
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-10-22 11:20:59 +0900

    Refact: BookRepository에서 existsById 제거

--- src/main/java/com/ureca/idle/idleapi/idleoriginapi/persistence/book/BookRepository.java
+++ src/main/java/com/ureca/idle/idleapi/idleoriginapi/persistence/book/BookRepository.java
@@ -4,5 +4,4 @@ import com.ureca.idle.idlejpa.book.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    boolean existsById(Long id);
 }
