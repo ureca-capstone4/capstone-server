84eea403d4a6fed1690ec66f8f08b9b7a183c3c7
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-10-24 10:45:49 +0900

    Refact : 성향 히스토리 조회용 파일들 생성

---
+++ src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/KidsPersonalityChangeHistory/dto/GetKidsPersonalityChangeHistoryResp.java
@@ -0,0 +1,6 @@
+package com.ureca.idle.idleapi.idleoriginapi.business.KidsPersonalityChangeHistory.dto;
+
+import java.util.List;
+
+public record GetKidsPersonalityChangeHistoryResp(List<GetKidsPersonalityHistoryResp> history) {
+}
---
+++ src/main/java/com/ureca/idle/idleapi/idleoriginapi/business/KidsPersonalityChangeHistory/dto/GetKidsPersonalityHistoryResp.java
@@ -0,0 +1,5 @@
+package com.ureca.idle.idleapi.idleoriginapi.business.KidsPersonalityChangeHistory.dto;
+
+public record GetKidsPersonalityHistoryResp(Long historyId, int ei, int sn, int tf, int jp, String mbti) {
+
+}
