2977b1f2e85dfd8cebb87d6d6f9b039dfeb7984e
seojeongwoo <qw4643987@naver.com>
2024-10-28 15:02:12 +0900

    feat: consumer 모듈 분리

---
+++ idle-fcfs-consumer/src/main/java/com/ureca/idle/fcfsconsumer/IdleFcfsConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.ureca.idle.fcfsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleFcfsConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IdleFcfsConsumerApplication.class, args);
+    }
+}
