414052dd29910f6738532809827995b15fedf81b
seojeongwoo <qw4643987@naver.com>
2024-10-28 15:02:54 +0900

    feat: batch 모듈 분리

---
+++ idle-batch/src/main/java/com/ureca/idle/batch/IdleBatchApplication.java
@@ -0,0 +1,11 @@
+package com.ureca.idle.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IdleBatchApplication.class, args);
+    }
+}
