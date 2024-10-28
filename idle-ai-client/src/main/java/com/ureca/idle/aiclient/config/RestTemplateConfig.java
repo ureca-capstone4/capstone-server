6a12d354120e0f96c5fe5e0eecb5933e6859ec7a
seojeongwoo <qw4643987@naver.com>
2024-10-28 15:03:09 +0900

    feat: ai client 모듈 분리

--- idle-ai-client/src/main/java/com/ureca/idle/aiclient/config/RestTemplateConfig.java
+++ idle-ai-client/src/main/java/com/ureca/idle/aiclient/config/RestTemplateConfig.java
@@ -1,5 +1,8 @@
 package com.ureca.idle.aiclient.config;
 
+import com.ureca.idle.aiclient.business.AiClientManagingUseCase;
+import com.ureca.idle.aiclient.implementation.AiClientManager;
+import com.ureca.idle.aiclient.implementation.LgAiManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
