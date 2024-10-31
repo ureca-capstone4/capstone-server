92a8cf16d4598e0a74b07476b61933d4e04ca14d
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-10-31 09:15:53 +0900

    Feat: 예외처리 로직 추가

---
+++ src/main/java/com/ureca/idle/batch/kidspersonalitybatch/KidsPersonalityBatchException.java
@@ -0,0 +1,11 @@
+package com.ureca.idle.batch.kidspersonalitybatch;
+
+import com.ureca.idle.exception.BaseException;
+import com.ureca.idle.exception.ExceptionType;
+
+public class KidsPersonalityBatchException extends BaseException {
+
+    public KidsPersonalityBatchException(ExceptionType exceptionType) {
+        super(exceptionType);
+    }
+}
