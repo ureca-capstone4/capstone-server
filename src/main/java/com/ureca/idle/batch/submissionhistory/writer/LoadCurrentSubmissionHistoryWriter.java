1f1e5e0d564b083880f9ef601d47e84973647c10
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-11-01 11:28:19 +0900

    Refact: 응모 히스토리 기본 배치 방식을 벌크 인서트로 변경

--- src/main/java/com/ureca/idle/batch/submissionhistory/writer/LoadCurrentSubmissionHistoryWriter.java
+++ src/main/java/com/ureca/idle/batch/submissionhistory/writer/LoadCurrentSubmissionHistoryWriter.java
@@ -22,49 +22,49 @@ public class LoadCurrentSubmissionHistoryWriter implements ItemWriter<CurrentRou
     private EntityManager entityManager;
     private final SubmissionHistoryBatchManager submissionHistoryBatchManager;
 
-//    @Override
-//    public void write(Chunk<? extends CurrentRoundSubmission> chunk) {
-//
-//        String sql = "INSERT INTO previous_round_submission " +
-//                "(user_id, name, phone_num) VALUES ";
-//        StringBuilder values = new StringBuilder();
-//
-//        List<? extends CurrentRoundSubmission> currentRoundSubmissionList = chunk.getItems();
-//        for (CurrentRoundSubmission currentRoundSubmission : currentRoundSubmissionList) {
-//            submissionHistoryBatchManager.savePreviousRoundSubmissionHistory(currentRoundSubmission, values);
-//        }
-//
-//        if (values.length() > 0) {
-//            sql += values.substring(0, values.length() - 2) + ";"; // 마지막 두 문자(쉼표와 공백) 제거
-//            entityManager.createNativeQuery(sql).executeUpdate();
-//        }
-//
-//        for (CurrentRoundSubmission currentRoundSubmission : currentRoundSubmissionList) {
-//            log.info("성공적으로 이전 차수 응모 테이블에 저장 완료되었습니다" + currentRoundSubmission.getName());
-//        }
-//    }
-
-
     @Override
     public void write(Chunk<? extends CurrentRoundSubmission> chunk) {
+
+        String sql = "INSERT INTO previous_round_submission " +
+                "(user_id, name, phone_num) VALUES ";
+        StringBuilder values = new StringBuilder();
+
         List<? extends CurrentRoundSubmission> currentRoundSubmissionList = chunk.getItems();
+        for (CurrentRoundSubmission currentRoundSubmission : currentRoundSubmissionList) {
+            submissionHistoryBatchManager.savePreviousRoundSubmissionHistory(currentRoundSubmission, values);
+        }
+
+        if (values.length() > 0) {
+            sql += values.substring(0, values.length() - 2) + ";"; // 마지막 두 문자(쉼표와 공백) 제거
+            entityManager.createNativeQuery(sql).executeUpdate();
+        }
 
         for (CurrentRoundSubmission currentRoundSubmission : currentRoundSubmissionList) {
-            try {
-                // 빌더 패턴을 사용하여 PreviousRoundSubmission 객체 생성
-                PreviousRoundSubmission previousRoundSubmission = PreviousRoundSubmission.builder()
-                        .userId(currentRoundSubmission.getUserId())
-                        .name(currentRoundSubmission.getName())
-                        .phoneNum(currentRoundSubmission.getPhoneNum())
-                        // 필요한 다른 필드 추가
-                        .build();
-
-                // 새로운 엔티티로 persist
-                entityManager.persist(previousRoundSubmission);
-                log.info("새로운 이전 차수 응모 테이블에 저장 완료되었습니다: " + previousRoundSubmission.getName());
-            } catch (Exception e) {
-                log.error("저장 중 오류 발생: " + e.getMessage(), e);
-            }
+            log.info("성공적으로 이전 차수 응모 테이블에 저장 완료되었습니다" + currentRoundSubmission.getName());
         }
     }
+
+
+//    @Override
+//    public void write(Chunk<? extends CurrentRoundSubmission> chunk) {
+//        List<? extends CurrentRoundSubmission> currentRoundSubmissionList = chunk.getItems();
+//
+//        for (CurrentRoundSubmission currentRoundSubmission : currentRoundSubmissionList) {
+//            try {
+//                // 빌더 패턴을 사용하여 PreviousRoundSubmission 객체 생성
+//                PreviousRoundSubmission previousRoundSubmission = PreviousRoundSubmission.builder()
+//                        .userId(currentRoundSubmission.getUserId())
+//                        .name(currentRoundSubmission.getName())
+//                        .phoneNum(currentRoundSubmission.getPhoneNum())
+//                        // 필요한 다른 필드 추가
+//                        .build();
+//
+//                // 새로운 엔티티로 persist
+//                entityManager.persist(previousRoundSubmission);
+//                log.info("새로운 이전 차수 응모 테이블에 저장 완료되었습니다: " + previousRoundSubmission.getName());
+//            } catch (Exception e) {
+//                log.error("저장 중 오류 발생: " + e.getMessage(), e);
+//            }
+//        }
+//    }
 }
