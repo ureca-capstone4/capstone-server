1c206d9accbd570ab4cd6f3bd123662ea43ae8a4
wewee <90890216+weejinyoung@users.noreply.github.com>
2024-11-03 21:52:01 +0900

    [ID-93] idle-server 에서 consumer 제거 (#52)
    * feat: delete consumer
    
    * env: delete sqs dependencies

--- src/main/java/com/ureca/idle/fcfsconsumer/consumer/CurrentRoundSubmissionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ureca.idle.fcfsconsumer.consumer;
-
-import com.ureca.idle.jpa.submission.CurrentRoundSubmission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrentRoundSubmissionRepository extends JpaRepository<CurrentRoundSubmission, Long> {
-
-}
--- src/main/java/com/ureca/idle/fcfsconsumer/consumer/SubmissionConsumer.java
+++
@@ -1,9 +0,0 @@
-package com.ureca.idle.fcfsconsumer.consumer;
-
-
-import java.util.List;
-
-public interface SubmissionConsumer {
-
-    void consumeSubmissions(List<Submission> submissions);
-}
