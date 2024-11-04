6eeca9c976e7eab9be9466baf3e91f02f89c8ffe
chaewon KIM <158552381+PeindreLaColline@users.noreply.github.com>
2024-11-04 12:10:10 +0900

    Update PersonalityQuestionController.java

--- src/main/java/com/ureca/idle/originapi/presentation/controller/personalityQuestion/PersonalityQuestionController.java
+++ src/main/java/com/ureca/idle/originapi/presentation/controller/personalityQuestion/PersonalityQuestionController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/personality/questions")
-@CrossOrigin("https://urecapstone.netlify.app")
 @RequiredArgsConstructor
 public class PersonalityQuestionController {
     private final PersonalityQuestionManagingUseCase personalityQuestionManagingUseCase;
